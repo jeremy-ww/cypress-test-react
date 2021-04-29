b8119594a4cb75865c0c2693c277a086032fd7c7
Jeremy Wu <jeremy.wu01@sap.com>
2021-04-29 18:23:55 +0800

    tweak

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import { mount } from "@cypress/react";
 import App from "./App";
 
+beforeEach(() => {
+  cy.intercept("GET", "https://www.example.com/widgets", {
+    statusCode: 200,
+    body: "it worked!",
+  });
+});
+
 it("renders learn react link", () => {
   mount(<App />);
   cy.get("div").contains("2");
+
+  /* eslint-disable */
+  fetch("https://www.example.com/widgets")
+    .then((res) => res.text())
+    .then((res) => {
+      console.log(res);
+    });
 });
