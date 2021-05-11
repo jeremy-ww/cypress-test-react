48bf671accc425fa9370753ac62270e48b5e1809
Jeremy Wu <jeremy.wu01@sap.com>
2021-05-11 16:31:38 +0800

    test

--- src/specs/App.test.tsx
+++ src/specs/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { mount } from "@cypress/react";
-import App from "./App";
+import App from "../App";
 
 beforeEach(() => {
   cy.intercept("GET", "https://www.example.com/widgets", {
